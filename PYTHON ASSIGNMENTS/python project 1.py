6ddc655bd44e68750ce682d40b97a72ed7f8cac5
Ham-zah18 <149466818+Ham-zah18@users.noreply.github.com>
2023-10-31 18:55:12 +0100

    Add files via upload

---
+++ PYTHON ASSIGNMENTS/python project 1.py
@@ -0,0 +1,154 @@
+print("hello world, welcome to python")
+print("  **    **")
+print("  **  * **")
+print("  ** * * **")
+
+
+
+print("  ********")
+print("  ********")
+print("  **")
+print("  *****")
+print("  *****")
+print("  **")
+print("  ********")
+print("  ********")
+
+
+
+print("  ***")
+print("  ***")
+print("  ***")
+print("  ***")
+print("  ***********")
+print("  ***********")
+
+
+
+print("  **********")
+print("  **********")
+print("  **")
+print("  **")
+print("  **")
+print("  **********")
+print("  **********")
+
+
+
+print("  **********")
+print("  **********")
+print("  **      **")
+print("  **      **")
+print("  **      **")
+print("  **********")
+print("  **********")
+
+
+
+print("  **************")
+print("  **************")
+print("  ***   **   ***")
+print("  ***   **   ***")
+print("  ***   **   ***")
+print("  ***   **   ***")
+
+
+
+print("  ********")
+print("  ********")
+print("  **")
+print("  *****")
+print("  *****")
+print("  **")
+print("  ********")
+print("  ********")
+
+
+
+print("  *************")
+print("  *************")
+print("       ***     ")
+print("       ***     ")
+print("       ***     ")
+print("       ***     ")
+print("       ***     ")
+
+
+
+print("  **********")
+print("  **********")
+print("  **      **")
+print("  **      **")
+print("  **      **")
+print("  **********")
+print("  **********")
+
+
+
+print("  ************")
+print("  ************")
+print("  **        **")
+print("  **        **")
+print("  **        **")
+print("  ************")
+print("  ************")
+print("  ***")
+print("  ***")
+print("  ***")
+print("  ***")
+
+
+
+print("  ***       ***")
+print("  ***       ***")
+print("  ***       ***")
+print("  *************")
+print("  *************")
+print("        **     ")
+print("        **     ")
+print("        **     ")
+print("        **     ")
+print("        **     ")
+
+
+
+print("  *************")
+print("  *************")
+print("       ***     ")
+print("       ***     ")
+print("       ***     ")
+print("       ***     ")
+print("       ***     ")
+
+
+
+print("  **         **")
+print("  **         **")
+print("  **         **")
+print("  *************")
+print("  *************")
+print("  **         **")
+print("  **         **")
+print("  **         **")
+
+
+
+
+print("  **********")
+print("  **********")
+print("  **      **")
+print("  **      **")
+print("  **      **")
+print("  **********")
+print("  **********")
+
+
+
+print("  *************")
+print("  *************")
+print("  ***       ***")
+print("  ***       ***")
+print("  ***       ***")
+print("  ***       ***")
+print("  ***       ***")
+print("  ***       ***")
